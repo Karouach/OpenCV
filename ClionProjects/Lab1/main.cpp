a668785ee1614078d626244d8a490a9fe70bad59
Karouach <guskarib@student.gu.se>
2015-09-10 22:45:54 +0200

    OpenCV C++ Displaying Frames of a Video

--- ClionProjects/Lab1/main.cpp
+++ ClionProjects/Lab1/main.cpp
@@ -1,16 +1,22 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
-//#include <opencv/highgui.h>
-
 
 int main( int argc, char** argv )
 {
-    IplImage * img = cvLoadImage(argv[1]);
-    cvNamedWindow("blob", CV_WINDOW_AUTOSIZE);
-    cvShowImage("blob", img);
-    cvWaitKey(0);
-    cvReleaseImage(&img);
-    cvDestroyWindow("blob");
+    cvNamedWindow("blob1", 0);
+    CvCapture* capture = cvCreateFileCapture(argv[1]);
+    IplImage* frame;
+    while (1){
+        frame = cvQueryFrame(capture);
+        if(! frame)break;
+        cvShowImage("blob1", frame);
+        char c = cvWaitKey(33);
+        if(c == 27) break;
+
+
+    }
+    cvReleaseCapture(&capture);
+    cvDestroyWindow("blob1");
     return 0;
 
 }
